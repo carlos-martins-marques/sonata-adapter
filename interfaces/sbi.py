59d8715701c0d0e2d82c98f6f90d0c41fa5d0e8b
carlos-martins-marques <carlos-martins-marques@telecom.pt>
2021-11-17 19:04:15 +0000

    change get_info parameters to string

--- interfaces/sbi.py
+++ interfaces/sbi.py
@@ -224,7 +224,7 @@ def ws_get_info(sliceUuid):
 
   messageDict = client_ws_thread(dict_message)
   if 'parameters' in messageDict:
-    return (messageDict['parameters'])
+    return (json.dumps(messageDict['parameters']))
   else:
     return messageDict
 
